6ff94f5bf0cb0d63cc95bc34bfd79d38f3f9c3d9
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-08-29 00:30:35 +0100

    Started gun and pickup script

---
+++ Assets/Gun.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GunState
+{ 
+    
+}
+
+
+public class Gun : MonoBehaviour, IPickup
+{
+  PickupState _State = PickupState.Dropped;
+  Player _Owner = null;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+
+  }
+
+  public 
+
+  public void OnDrop(GameObject who)
+  {
+    _State = PickupState.Dropped;
+    _Owner = null;
+  }
+
+  public void OnPickup(GameObject who)
+  {
+    _State = PickupState.PickedUp;
+    _Owner = who.GetComponent<Player>();
+  }
+
+  public PickupState GetPickupState()
+  {
+    return _State;
+  }
+}
---
+++ Assets/IPickup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PickupState
+{ 
+ PickedUp,
+ Dropped,
+};
+
+public interface IPickup
+{ 
+  // _who ("who" picked the object up)
+  void OnPickup(GameObject who);
+
+  // _who ("who" dropped the object up)
+  void OnDrop(GameObject who);
+
+  // Returns whether or not the object has been picked up
+  PickupState GetPickupState();
+}
