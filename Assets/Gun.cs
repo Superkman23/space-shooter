e6b7700d4bb5cc39a6f53a29cdb26bb14aeee2e9
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-08-29 01:38:02 +0100

    Get gun pickup working

--- Assets/Gun.cs
+++ Assets/Gun.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour, IPickup
 {
   [SerializeField] GameObject _BulletObject = null;
+  [SerializeField] Vector2 _OffsetFromPlayer = Vector2.zero;
   PickupState _State = PickupState.Dropped;
 
   int _BulletsLeft = 10;
 
-  void OnTriggerEnter(Collider other)
+  void OnTriggerEnter2D(Collider2D other)
   {
     // If the thing colliding is the player
-    if (other.CompareTag("Player"))
+    if (other.CompareTag("Player") && _State == PickupState.Dropped)
     {
       // Trigger the pickup event
       OnPickup(other.gameObject);
@@ -22,6 +24,8 @@ public class Gun : MonoBehaviour, IPickup
   // Returns true if the gun is empty
   public bool Shoot()
   {
+    Debug.Log("Shooting gun");
+
     _BulletsLeft--;
     GameObject newBullet = Instantiate(_BulletObject,transform.position, transform.parent.rotation);
     newBullet.GetComponent<Rigidbody>().AddForce(newBullet.transform.forward * 10);
@@ -37,11 +41,16 @@ public class Gun : MonoBehaviour, IPickup
   public void OnDrop(GameObject who)
   {
     _State = PickupState.Dropped;
+
+    transform.parent = null;
   }
 
   public void OnPickup(GameObject who)
   {
     _State = PickupState.PickedUp;
+
+    transform.parent = who.transform;
+    transform.localPosition = _OffsetFromPlayer;
   }
 
   public PickupState GetPickupState()
