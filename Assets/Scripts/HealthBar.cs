73bf60bd0285da9a793e7143eb8863982bc849b8
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-08-29 01:03:29 -0400

    Added healthbar over player

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,10 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour {
   [SerializeField] Player _PlayerFocus = null;
   [SerializeField] Vector2 _PositionOffset = Vector2.up;
+  Image _Image;
+
+  private void Awake()
+  {
+    _Image = GetComponent<Image>();
+  }
+
 
   // Update is called once per frame
   void Update () {
