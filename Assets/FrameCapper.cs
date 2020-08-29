fa9ba5378eab77061afec7a99cc7cb468f62b27c
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-08-28 20:24:11 -0400

    added a frame limiter  to cap frames at 100

---
+++ Assets/FrameCapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameCapper : MonoBehaviour
+{
+  [SerializeField] int _MaxFPS;
+  // Start is called before the first frame update
+  void Start()
+  {
+    Application.targetFrameRate = _MaxFPS;
+  }
+
+}
