0d16ab503ed3b2740de9d2165c7a64a647c45e82
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-08-28 22:17:08 -0400

    Enabled the ability to hold the shoot key to fire

--- Assets/Scripts/PlayerGunController.cs
+++ Assets/Scripts/PlayerGunController.cs
@@ -3,17 +3,27 @@ using UnityEngine;
 public class PlayerGunController : MonoBehaviour {
   [Header ("Settings")]
   [SerializeField] KeyCode _ShootKey = default;
+  [SerializeField] float _HoldFireSpeed = 0.3f;
+  float _HoldFireTimeR;
+
 
   [HideInInspector]
   public Gun _Holding = null;
 
   // Update is called once per frame
   void Update () {
-    if (_Holding != null && Input.GetKeyDown (_ShootKey)) {
+    _HoldFireTimeR -= Time.deltaTime;
+    if(_Holding == null)
+    {
+      return;
+    }
+    if (Input.GetKeyDown (_ShootKey) || (Input.GetKey(_ShootKey) && _HoldFireTimeR <= 0)) {
+      _HoldFireTimeR = _HoldFireSpeed;
       // Shoot the gun, then check if there are any bullets left
-      if (_Holding.Shoot ()) {
+      if (_Holding.Shoot())
+      {
         // There are no bullets left, so throw the gun
-        _Holding.Throw ();
+        _Holding.Throw();
         _Holding = null;
       }
     }
