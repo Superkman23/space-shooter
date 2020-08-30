0e4682b35418dde5a6c585689eac1afa7f4cab13
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-08-29 21:35:28 -0400

    Revert "Format files"
    This reverts commit 16eac7cb17bc28e686ca10519d27962af4027696.

--- Assets/Scripts/PhysicsLink.cs
+++ Assets/Scripts/PhysicsLink.cs
@@ -1,17 +1,20 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using Mirror;
 using UnityEngine;
+using Mirror;
 
-public class PhysicsLink : NetworkBehaviour {
+public class PhysicsLink : NetworkBehaviour
+{
   public Rigidbody2D _Rigidbody;
   [SyncVar] public Vector2 _Position;
   [SyncVar] public Vector2 _Velocity;
   [SyncVar] public float _AngularVelocity;
   [SyncVar] public float _Rotation;
 
-  void FixedUpdate () {
-    if (isServer) {
+  void FixedUpdate()
+  {
+    if (isServer)
+    {
       _Position = _Rigidbody.position;
       _Rotation = _Rigidbody.rotation;
       _Velocity = _Rigidbody.velocity;
@@ -24,37 +27,37 @@ public class PhysicsLink : NetworkBehaviour {
     }
     else // Not server so must be client
     {
-      _Rigidbody.position = _Position + _Velocity * (float) NetworkTime.rtt;
-      _Rigidbody.rotation = _Rotation * _AngularVelocity * (float) NetworkTime.rtt;
+      _Rigidbody.position = _Position + _Velocity * (float)NetworkTime.rtt;
+      _Rigidbody.rotation = _Rotation * _AngularVelocity * (float)NetworkTime.rtt;
       _Rigidbody.velocity = _Velocity;
       _Rigidbody.angularVelocity = _AngularVelocity;
     }
   }
-
-  [Command] //function that runs on server when called by a client
-  public void CmdResetPose () {
-    _Rigidbody.position = new Vector3 (0, 1, 0);
-    _Rigidbody.velocity = new Vector3 ();
-  }
-  public void ApplyForce (Vector2 force, ForceMode2D FMode) //apply force on the client-side to reduce the appearance of lag and then apply it on the server-side
+  [Command]//function that runs on server when called by a client
+  public void CmdResetPose()
   {
-    _Rigidbody.AddForce (force, FMode);
-    CmdApplyForce (force, FMode);
+    _Rigidbody.position = new Vector3(0, 1, 0);
+    _Rigidbody.velocity = new Vector3();
   }
-  public void AddDirectForce (Vector2 force) //apply force on the client-side to reduce the appearance of lag and then apply it on the server-side
+  public void ApplyForce(Vector2 force, ForceMode2D FMode)//apply force on the client-side to reduce the appearance of lag and then apply it on the server-side
+  {
+    _Rigidbody.AddForce(force, FMode);
+    CmdApplyForce(force, FMode);
+  }
+  public void AddDirectForce(Vector2 force)//apply force on the client-side to reduce the appearance of lag and then apply it on the server-side
   {
     if (!hasAuthority) { return; }
     _Rigidbody.velocity += force;
-    CmdAddDirectForce (force);
+    CmdAddDirectForce(force);
   }
-
   [Command]
-  public void CmdApplyForce (Vector2 force, ForceMode2D FMode) {
-    _Rigidbody.AddForce (force, FMode); //apply the force on the server side
+  public void CmdApplyForce(Vector2 force, ForceMode2D FMode)
+  {
+    _Rigidbody.AddForce(force, FMode); //apply the force on the server side
   }
-
   [Command]
-  public void CmdAddDirectForce (Vector2 force) {
+  public void CmdAddDirectForce(Vector2 force)
+  {
     _Rigidbody.velocity += force;
   }
 
