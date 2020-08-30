c9c835cf2ff8cd1df1e2abda140beef216f8df04
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-08-30 17:44:31 -0400

    added a setvelocity command to physics link

--- Assets/Scripts/PhysicsLink.cs
+++ Assets/Scripts/PhysicsLink.cs
@@ -33,31 +33,53 @@ public class PhysicsLink : NetworkBehaviour
       _Rigidbody.angularVelocity = _AngularVelocity;
     }
   }
+
+  public void ApplyForce(Vector2 force, ForceMode2D FMode) //apply force on the client-side to reduce the appearance of lag and then apply it on the server-side
+  {
+    if (!hasAuthority) { return; }
+    _Rigidbody.AddForce(force, FMode);
+    CmdApplyForce(force, FMode);
+  }
+
+  public void AddDirectForce(Vector2 force) //Directly edit the rigidbody's velocity
+  {
+    if (!hasAuthority) { return; }
+    _Rigidbody.velocity += force;
+    CmdAddDirectForce(force);
+  }
+
+  public void SetVelocity(Vector2 velocity)
+  {
+    if (!hasAuthority) { return; }
+    _Rigidbody.velocity = velocity;
+    CmdSetVelocity(velocity);
+  }
+
+  #region Commands
+
+  [Command]
+  public void CmdApplyForce(Vector2 force, ForceMode2D FMode)
+  {
+    _Rigidbody.AddForce(force, FMode); //apply the force on the server side
+  }
+
+  [Command]
+  public void CmdAddDirectForce(Vector2 force)
+  {
+    _Rigidbody.velocity += force;
+  }
+
+  [Command]
+  public void CmdSetVelocity(Vector2 velocity)
+  {
+    _Rigidbody.velocity = velocity;
+  }
+
   [Command]
   public void CmdResetPose()
   {
     _Rigidbody.position = new Vector3(0, 1, 0);
     _Rigidbody.velocity = new Vector3();
   }
-  public void ApplyForce(Vector2 force, ForceMode2D FMode)//apply force on the client-side to reduce the appearance of lag and then apply it on the server-side
-  {
-    _Rigidbody.AddForce(force, FMode);
-    CmdApplyForce(force, FMode);
-  }
-  public void AddDirectForce(Vector2 force)//apply force on the client-side to reduce the appearance of lag and then apply it on the server-side
-  {
-    if (!hasAuthority) { return; }
-    _Rigidbody.velocity += force;
-    CmdAddDirectForce(force);
-  }
-  [Command]
-  public void CmdApplyForce(Vector2 force, ForceMode2D FMode)
-  {
-    _Rigidbody.AddForce(force, FMode); //apply the force on the server side
-  }
-  [Command]
-  public void CmdAddDirectForce(Vector2 force)
-  {
-    _Rigidbody.velocity += force;
-  }
+  #endregion
 }
